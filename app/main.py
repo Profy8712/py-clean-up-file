7d6d227f77471f6d14f25c10e63e5a8df25c0a68
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-08 20:44:13 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         # This method is called when entering the 'with' block.
         # You can optionally open the file here if needed.
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: type, exc_val: Exception, exc_tb: object) -> None:
         # This method is called when exiting the 'with' block.
         # Remove the file if it exists.
         if os.path.exists(self.filename):
